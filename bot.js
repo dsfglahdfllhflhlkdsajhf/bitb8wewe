004a703de1df45b6becca3ac6654f3dc858081c3
dsfglahdfllhflhlkdsajhf <42523498+dsfglahdfllhflhlkdsajhf@users.noreply.github.com>
2018-08-20 19:21:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const { Client, Util } = require('discord.js');
 const dateFormat = require('dateformat');
 const fs = require('fs');
 const prefix = "#";
